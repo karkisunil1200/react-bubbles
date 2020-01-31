849c92b5c79cb5e31f08db32b5b9dc5e7620913a
Sunil Karki <karkisunil1200@gmail.com>
2020-01-31 13:47:28 -0600

    Login page rendering

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, {useState} from 'react';
 
 const Login = () => {
+  const [state, setState] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleChanges = e => {
+    setState({...state, [e.target.name]: e.target.value});
+    // console.log([e.target.name], e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault;
+  };
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
+      <form>
+        <div className='form-input' onSubmit={handleSubmit}>
+          <label>username</label>
+          <input type='text' name='username' onChange={handleChanges} />
+        </div>
+
+        <div className='form-input'>
+          <label>password</label>
+          <input type='text' name='password' onChange={handleChanges} />
+        </div>
+        <button style={{margin: '20px', background: 'pink', width: '50px', alignSelf: 'center'}}>
+          Login
+        </button>
+      </form>
     </>
   );
 };
