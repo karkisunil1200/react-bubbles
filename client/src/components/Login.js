8500434e392e30707c30dbaef52caaf6d5a80388
Sunil Karki <karkisunil1200@gmail.com>
2020-01-31 10:04:37 -0600

    Initial commit

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 
 const Login = () => {
   // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+  // when you have handled the token, navigate to the BubblePage rout
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
