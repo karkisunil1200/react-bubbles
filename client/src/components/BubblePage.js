33b474c300fc903368b4c66d35f17b97b91cef98
Sunil Karki <karkisunil1200@gmail.com>
2020-01-31 14:19:11 -0600

    Data rendering in colors and bubbles

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,13 +1,23 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, {useState, useEffect} from 'react';
 
-import Bubbles from "./Bubbles";
-import ColorList from "./ColorList";
+import Bubbles from './Bubbles';
+import ColorList from './ColorList';
+import {axiosWithAuth} from '../utils/axiosWithAuth';
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get('/api/colors')
+      .then(res => {
+        console.log(res.data);
+        setColorList(res.data);
+      })
+      .catch();
+  }, []);
 
   return (
     <>
