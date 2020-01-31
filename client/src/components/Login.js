963f5ab5944986f019c4e38932ed283db39d564a
Sunil Karki <karkisunil1200@gmail.com>
2020-01-31 10:28:40 -0600

    PrivateRoute and Login done

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage rout
+
+  const [state, setState] = useState({
+    username: '',
+    password: ''
+  });
+
+  const changeHandler = e => {
+    setState({...state, [e.target.name]: e.target.value});
+    // console.log([e.target.name], e.target.value);
+  };
+
+  const submitHandler = e => {
+    e.preventDefault();
+
+    axios
+      .post('http://localhost:5000/api/login', state)
+      .then(response => {
+        console.log(response);
+        localStorage.setItem('token', response.data.payload);
+        props.history.push('/homepage');
+      })
+      .catch();
+  };
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <div className='login'>
+        <h3 className='title'>Login to your account</h3>
+        <form className='login-form' onSubmit={submitHandler}>
+          <div className='login-form-input'>
+            <label className='label'>Username</label>
+            <input type='text' name='username' value={state.username} onChange={changeHandler} />
+          </div>
+
+          <div className='login-form-input'>
+            <label className='label'>Password</label>
+            <input
+              type='password'
+              name='password'
+              value={state.password}
+              onChange={changeHandler}
+            />
+          </div>
+          <button className='btn'>Login</button>
+        </form>
+      </div>
     </>
   );
 };
---
+++ client/src/utils/PrivateRoute.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+const PrivateRoute = ({component: Component, ...rest}) => {
+  return (
+    <Route
+      render={props => {
+        if (localStorage.getItem('token')) {
+          return <Component {...props} />;
+        } else {
+          return <Redirect to='/login' />;
+        }
+      }}
+    />
+  );
+};
